020afbc6dcba206fa1d690e1d35753cb1c873170
rajeshoec <rajeshsahuoec96@gmail.com>
2021-11-06 14:43:35 +0530

    -first commit

---
+++ src/main/java/com/jenkinscicd/jenkins/controller/Student.java
@@ -0,0 +1,16 @@
+package com.jenkinscicd.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Student {
+
+	@GetMapping("/studentName")
+	public String getStudentName() {
+
+		return "Rajesh is here";
+
+	}
+
+}
